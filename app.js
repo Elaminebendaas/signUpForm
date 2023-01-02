2d5b4e16c8710f4bac1c573365411b9a07708130
Elaminebendaas <33202205+Elaminebendaas@users.noreply.github.com>
2023-01-02 10:23:25 -0500

    Fixed background image & +submit button

---
+++ app.js
@@ -0,0 +1,9 @@
+password = document.getElementById('password')
+repassword = document.getElementById('repassword')
+submitButton = document.getElementById('submit')
+
+submitButton.addEventListener('click', passwordCheck)
+
+function passwordCheck(){
+    
+}
