5d5ce627be1684d22db467054e404f5fe1a3d1f4
Elaminebendaas <33202205+Elaminebendaas@users.noreply.github.com>
2023-01-02 15:12:34 -0500

    Completed (Styling incomplete)

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-password = document.getElementById('password')
-repassword = document.getElementById('repassword')
-submitButton = document.getElementById('submit')
+let password = document.getElementById('password')
+let repassword = document.getElementById('repassword')
+let submitButton = document.getElementById('submit')
+let failedText = document.getElementById('failedtext')
+
 
 submitButton.addEventListener('click', passwordCheck)
 
 function passwordCheck(){
-    
+    if(password.value !== repassword.value){
+        document.getElementById('passLabel1').classList.add('failedtext')
+        document.getElementById('passLabel2').classList.add('failedtext')
+        failedText.innerText = "Passwords do not match"
+        event.preventDefault()
+    }else{
+        alert('You have signed up!')
+    }
 }
